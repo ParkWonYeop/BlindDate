5054f39659d7e8a17aab3d0896c0487aba2e73ba
ParkWonYeop <pwy9679@gnu.ac.kr>
2024-07-07 00:46:01 +0900

    2024-07-07/main
    - creat project

---
+++ src/test/kotlin/org/dblab/blinddate/BlindDateApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dblab.blinddate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlindDateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
